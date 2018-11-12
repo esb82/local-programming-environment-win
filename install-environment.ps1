e3be802b28ae19c57449d34a788c8d6406f12263
Emil <emil.brunner@gmx.de>
2018-11-12 06:38:46 +0100

    initial commit
    initial commit

---
+++ install-environment.ps1
@@ -0,0 +1,11 @@
+# check Is Principal / Administrator
+$identity = [Security.Principal.WindowsIdentity]::GetCurrent()
+$principal = new-object Security.Principal.WindowsPrincipal $identity
+if ($principal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator) -eq $false) {
+	$psArgs = '-noprofile -nologo -executionpolicy bypass -file "{0}"' -f $MyInvocation.MyCommand.Path
+	Start-Process -FilePath 'powershell.exe' -ArgumentList $psArgs -Verb RunAs
+	exit
+}
+	
+	
+Get-ComputerInfo -Property Windows*
